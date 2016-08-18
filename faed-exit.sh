98421bd96dbae3c13c57c7a7d2e863d0495af5f1
xavics <xacosa@gmail.com>
2016-08-18 10:04:15 +0200

    Exit FAED script, edited

--- faed-exit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sshpass -p 'lqgalaxy' ssh killall python
-sshpass -p 'lqgalaxy' ssh lg@$galaxy_ip \"echo '' > /var/www/html/kmls.txt\"
-sshpass -p 'lqgalaxy' ssh lg@$galaxy_ip \"echo '' > /var/www/html/kmls_4.txt\"
